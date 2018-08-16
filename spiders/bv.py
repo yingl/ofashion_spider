d058bc92b0fc1b1cbeaa4d3ea44885683274265b
yingl <linying_43151@163.com>
2018-08-16 22:48:00 +0800

    ...

---
+++ spiders/bv.py
@@ -0,0 +1,43 @@
+import sys
+sys.path.append('../')
+import of_spider
+import of_utils
+
+class BV(of_spider.Spider):
+    def parse_entry(self, driver):
+        product_count = 0
+        while True:
+            elements = of_utils.find_elements_by_css_selector(driver, 'div.products > div.item > div.itemWrapper > a')
+            if len(elements) > product_count:
+                product_count = len(elements)
+                driver.execute_script('window.scrollBy(0, document.body.scrollHeight);')
+                of_utils.sleep(4)
+            else:
+                break
+        return [element.get_attribute('href').strip() for element in elements]
+
+    def parse_product(self, driver):
+        product = of_spider.empty_product.copy()
+        # title
+        element = of_utils.find_element_by_css_selector(driver, 'span.inner.modelName')
+        if element:
+            product['title'] = element.text.strip()
+        else:
+            raise Exception('Title not found')
+        # code
+        element = of_utils.find_element_by_css_selector(driver, 'div.modelFabricColor > span.value')
+        if element:
+            product['code'] = element.text.strip()
+        # price_cny
+        element = of_utils.find_element_by_css_selector(driver, 'div.convertedPrice > span.price')
+        if element:
+            price_text = element.text.strip()[1:].strip().replace(',', '') # 去掉开头的¥
+            product['price_cny'] = int(float(price_text))
+        # images
+        elements = of_utils.find_elements_by_css_selector(driver, 'ul.alternativeImages > li.swiper-slide > img')
+        images = [element.get_attribute('src').strip().replace('_7_', '_16_') for element in elements]
+        product['images'] = ';'.join(images)
+        # detail
+        element = of_utils.find_element_by_css_selector(driver, 'div.jspContainer> div.jspPane > span.value')
+        product['detail'] = element.text.strip()
+        return product
---
+++ spiders/gucci.py
@@ -0,0 +1,38 @@
+import sys
+sys.path.append('../')
+import of_spider
+import of_utils
+
+class Gucci(of_spider.Spider):
+    def parse_entry(self, driver):
+        btn = of_utils.find_element_by_css_selector(driver, 'div.spice-looks-grid-button > a')
+        if btn:
+            driver.execute_script('arguments[0].click();', btn) # 点击“浏览所有”
+        elements = of_utils.find_elements_by_css_selector(driver, 'ul.spice-float-clearfix > li > div > div > a.spice-item-grid')
+        return [element.get_attribute('href').strip() for element in elements]
+
+    def parse_product(self, driver):
+        product = of_spider.empty_product.copy()
+        # title
+        element = of_utils.find_element_by_css_selector(driver, 'h1.spice-product-name')
+        if element:
+            product['title'] = element.text.strip()
+        else:
+            raise Exception('Title not found')
+        # code
+        element = of_utils.find_element_by_css_selector(driver, 'div.spice-style-number-title > span')
+        if element:
+            product['code'] = element.text.strip()
+        # price_cny
+        element = of_utils.find_element_by_css_selector(driver, 'div.spice-product-price > span')
+        if element:
+            price_text = element.text.strip()[1:].strip().replace(',', '') # 去掉开头的¥
+            product['price_cny'] = int(float(price_text))
+        # images
+        elements = of_utils.find_elements_by_css_selector(driver, 'img.spice-smart-zoom')
+        images = list(set([element.get_attribute('src').strip() for element in elements]))
+        product['images'] = ';'.join(images)
+        # detail
+        element = of_utils.find_element_by_css_selector(driver, 'div.spice-description-small > p')
+        product['detail'] = element.get_attribute('innerHTML').strip()
+        return product
