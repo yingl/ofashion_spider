2ac1139a0aeaa716d59ec5d34c54b9df292ddd79
yingl <linying_43151@163.com>
2019-03-23 13:09:12 +0800

    ...

---
+++ spiders/givenchybeauty.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append('../')
+import of_spider
+import of_utils
+
+class GivenchyBeauty(of_spider.Spider):
+    def parse_entry(self, driver):
+        elements = of_utils.find_elements_by_css_selector(driver, 'article.giv-ProductTile-item.js-ProductItem > a')
+        return [element.get_attribute('href').strip() for element in elements]
+
+    def parse_product(self, driver):
+        product = of_spider.empty_product.copy()
+        # title
+        element = of_utils.find_element_by_css_selector(driver, 'h1.product-name')
+        if element:
+            product['title'] = element.text.strip()
+        else:
+            raise Exception('Title not found')
+        # code
+        element = of_utils.find_element_by_css_selector(driver, 'span[itemprop=productID]')
+        if element:
+            product['code'] = element.text.strip()
+        # price_cny N/A
+        # images
+        elements = of_utils.find_elements_by_css_selector(driver, 'div.slick-track > a > picture > img')
+        images = [element.get_attribute('src').strip() for element in elements]
+        product['images'] = ';'.join(images)
+        # detail
+        element = of_utils.find_element_by_css_selector(driver, 'div.giv-ProductDescription-contentDetail-Detail > div.giv-ProductDescription-contentDetail-Detail-contentText > p')
+        product['detail'] = element.get_attribute('innerHTML').strip()
+        return product
